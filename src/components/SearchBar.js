48efb3504ace1f65eaae01b39393ea8120ae847e
Arison13 <arisonarias@gmail.com>
2022-03-04 00:03:49 -0500

    better changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,23 +2,22 @@ import React from 'react'
 import './SearchBar.css'
 
 function SearchBar(props) {
+  
   return (
    <form onSubmit={props.handleSubmit}> 
       <div className='bar'>
-        <input placeholder='YYYY-MM-DD' 
-          value={props.dateInput} onInput={e => props.setDateInput(e.target.value)} 
+        <input placeholder='Enter Date' 
+          value={props.dateInput} 
+          onInput={e => props.setDateInput(e.target.value)} 
           onSubmit={props.handleSubmit}
           type="date"/>
       </div>
-        
-        <div className='btns'>
-          <button> Search </button> 
-          <button onClick={props.handleSubmit}> Get Today</button>
-        </div>
-        
+      <div className='btns'>
+        <button> Search </button> 
+        <button onClick={props.handleSubmit}> Get Today</button>
+      </div>
         <p> <span>Must be after 1995-06-16</span></p>
     </form>
-
   )
 }
 
