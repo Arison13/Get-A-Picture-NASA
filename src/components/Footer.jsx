a83783fe5b65ae31d7530d8bca553dbc12c6d3c5
Arison13 <arisonarias@gmail.com>
2022-09-07 00:41:01 -0400

    fix footer styling bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
+
 export default function Footer() {
 
     return (
-        <footer> 
+      <footer> 
         <p> Made By 
           <a className='linkedIn' href='https://www.linkedin.com/in/arisonarias/' target="_blank" rel="noreferrer"> Arison Arias </a>
         </p>
--- src/components/MainInfo.js
+++ src/components/MainInfo.js
@@ -7,22 +7,20 @@ function MainInfo({state}) {
   
   return (
     <div className='main'> 
-    <div className='main-info'> 
-      <a className="image-link" href={state.url} target="_blank" rel="noreferrer">
-        { 
-          state.media_type === "video" 
-          ? <VideoPlayer state={state}/>
-          : <img src={state.hdurl} alt={state.title}/> 
-        }
-      </a>
-
-      <div className='explanation-section'> 
-          <p className='title'> {state.title}</p>
-          <p className='explanation'> {state.explanation}</p>
-          <p className='date'>{state.date}</p>
-      </div> 
-
-    </div>
+      <div className='main-info'> 
+        <a className="image-link" href={state.url} target="_blank" rel="noreferrer">
+          { 
+            state.media_type === "video" 
+            ? <VideoPlayer state={state}/>
+            : <img src={state.hdurl} alt={state.title}/> 
+          }
+        </a>
+        <div className='explanation-section'> 
+            <p className='title'> {state.title}</p>
+            <p className='explanation'> {state.explanation}</p>
+            <p className='date'>{state.date}</p>
+        </div> 
+      </div>
       <Footer/>
     </div>
         
